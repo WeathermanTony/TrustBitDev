f6a9a23ab5a95ef42d72c11388bb003d7ec5056c
Tony Cirigliano <tony@cirigliano.com>
2025-06-27 03:16:41 +0000

    Verify TrustBitDev repository

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import ssl
 app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'TrustBit Dev Server'
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain('/home/trustbitdev/certs/selfsigned.crt', '/home/trustbitdev/certs/selfsigned.key')
+    app.run(host='0.0.0.0', port=8000, ssl_context=context)
